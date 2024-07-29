93126efa5397ea442a7e66fa0c378d8c73444dd9
Asma Mohammad <asma.md130804@gmail.com>
2024-07-29 10:50:12 -0700

    Add files via upload

---
+++ Bookmyshow clone/js/data.js
@@ -0,0 +1,17 @@
+let movies = [
+    {
+        image: 'img/banner/banner1.png'
+    },
+    {
+        image: 'img/banner/banner2.png'
+    },
+    {
+        image: 'img/banner/banner3.webp'
+    },
+    {
+        image: 'img/banner/banner4.webp'
+    },
+    {
+        image: 'img/banner/banner5.png'
+    }
+]
